23c9d7ac7e8cff65cfdb8b047fb99eff595ffc2a
hyeonuk <guyeonuk@naver.com>
2023-04-28 14:18:48 +0900

    hyeonuk 수정

--- src/main/Main.java
+++ src/main/Main.java
@@ -24,11 +24,25 @@ class phone {
 		System.out.println("전화기능");
 	}
 }
+class Iphone extends phone {
+	
+	public void faceId() {
+		System.out.println("faceId");
+	}
+	public void faceTime() {
+		System.out.println("faceTime");
+	}
+	public void applePay() {
+		System.out.println("applePay");
+	}
+	
+}
 
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		Iphone ip = new Iphone();
 
 		while (true) {
 			System.out.println("1. 아이폰");
@@ -40,6 +54,14 @@ public class Main {
 			switch (choice) {
 			case 1:
 				System.out.println("아이폰 가능한 기능 보기");
+				ip.on();
+				ip.faceId();
+				ip.faceTime();
+				ip.applePay();
+				ip.SN("1234");
+				ip.message();
+				ip.call();
+				ip.off();
 				break;
 			case 2:
 				System.out.println("삼성 가능한 기능 보기");
